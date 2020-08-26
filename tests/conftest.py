cce0536f863c1073ba050940c5e1530ed183f1bb
Leonardo Rochael Almeida <leorochael@gmail.com>
2020-08-26 14:40:19 -0300

    In tmpdir() fixture, use our Path to check isdir()

--- tests/conftest.py
+++ tests/conftest.py
@@ -142,8 +142,10 @@ def tmpdir(request, tmpdir):
     to return our typical path object instead of py.path.local as well as
     deleting the temporary directories at the end of each test case.
     """
+    tmpdir_str = str(tmpdir)
+    tmpdir = Path(tmpdir_str)
     assert tmpdir.isdir()
-    yield Path(str(tmpdir))
+    yield tmpdir
     # Clear out the temporary directory after the test has finished using it.
     # This should prevent us from needing a multiple gigabyte temporary
     # directory while running the tests.
